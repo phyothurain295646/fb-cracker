a9349701d1a7391569904a8ae869c9a3ed2d5bc9
ۥٰ ۥٰ <88605415+phyothurain295646@users.noreply.github.com>
2021-08-11 18:50:12 +0630

    Fb-Cracker.py

---
+++ Fb-Cracker.py
@@ -0,0 +1,30 @@
+# fb-cracker
+
+[x]THIS IS FOR TERMUX USER ONLY[x]
+
+AUTO FACEBOOK CRACKER
+
+
+<h2>INSTALLATION:</h2>
+
+<p>pkg update</p>
+
+<p>pkg install git</p>
+
+<p>pkg install python</p>
+
+pkg install python2
+
+git clone https://github.com/phyothurain295646/Phyo-Thu-Rain.git
+
+cd fb-cracker
+
+pip2 install -r requirements.txt
+
+python Fb-Cracker.py
+
+
+
+
+
+
